a3ea70105a1d8844b1db9e39807222dc7dae4e14
Aaron Hallaert <a.hallaert@televic.com>
2025-02-07 15:09:24 +0100

    elinux test

---
+++ elinux/flutter/main.dart
@@ -0,0 +1,12 @@
+//
+// Generated file. Do not edit.
+//
+// @dart=3.6
+
+import 'package:flutter_mmu/main.dart' as entrypoint;
+import 'generated_plugin_registrant.dart';
+
+Future<void> main() async {
+  registerPlugins();
+  entrypoint.main();
+}
