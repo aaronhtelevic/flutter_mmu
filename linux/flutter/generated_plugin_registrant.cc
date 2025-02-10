087094222a0aeae129d08033dc5b7487b45dd747
Aaron Hallaert <a.hallaert@televic.com>
2025-02-10 10:54:07 +0100

    add video player linux

--- linux/flutter/generated_plugin_registrant.cc
+++ linux/flutter/generated_plugin_registrant.cc
@@ -6,6 +6,10 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <video_player_linux/video_player_linux.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) video_player_linux_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "VideoPlayerLinux");
+  video_player_linux_register_with_registrar(video_player_linux_registrar);
 }
